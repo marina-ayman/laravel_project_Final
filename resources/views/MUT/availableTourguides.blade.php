71292ec78710b7835d4e71d30534fb8e9e40d8f6
Marina Ayman <99688015+marina-ayman@users.noreply.github.com>
2023-03-16 02:37:23 -0700

    An

--- resources/views/MUT/availableTourguides.blade.php
+++ resources/views/MUT/availableTourguides.blade.php
@@ -1,6 +1,7 @@
 
 
-
+@extends("temp")
+@section('bodyContent') 
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,98 +9,66 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('./assets/css/Cards/placesCards.css')}}" >
-    <script src="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js')}}" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-    <title>Document</title>
+    <!--  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+
+<link rel="stylesheet" href="{{asset('assets/css/hotels.css')}}" >
+    <title>Hotels</title>
 </head>
 <body>
 
 
-<link rel="stylesheet" href="{{asset('assets/css/Cards/tourguideCards.css')}}">
-<section class="blog top mt-3" id="blog">
-    <div class="container">
-      <div class="heading">
-        <h5>OUR TOURGUIDE</h5>
-        <h3>Discover your best trip in Aswan</h3>
-      </div>
-
-      <div style="text-align: center;" class="mb-4">
-        <button class="button">TOURGUIDE</button>
-     </div>
-
-
-
-        <div class="container tourguideCard">
-          <div class="row">
-          <div class="row g-5" style="margin-top: 1rem;" >
-              @foreach ($availableTourguides as $Tourguide)
-                      <div class="col-lg-4  col-sm-6 col-12 rounded">
-                          <div class="card border-0 me-lg-4 mb-lg-0 mb-4 ">
-                              <div class="backgroundEffect"></div>
-                              <div class="pic">
-                                  <img class="" src="  'img/'.{{ asset($Tourguide->User->image) }}" alt="">
-                                  <div class="date">
-                                      <a href="{{route('bookWithTourguide',['order'=>$order,'tourguide'=>$Tourguide->id])}}"><span class="day">Book now!</span></a>
-                                  </div>
-                              </div>
-                              <div class="content">
-                                  <h2>{{$Tourguide->User->name}}</h2>
-                                  <p class="text-muted mt-3">price {{$Tourguide->price_per_day}} /day</p>
-                                      {{-- @foreach($Tourguide->languages as $item)
-                                          <h6 class="h-2 mt-4"><br>{{$item['language']}} </h6>
-                                      @endforeach  --}}
-                                  <p class="description" style="color:black">Language : </p>
-                                  @foreach( $Tourguide->languages as $lang)
-                                  <h1>  {{$lang->language}}</h1>
-                                    @endforeach
-                                  <h6 class="h-2 mt-4"><br> Bio:{{isset($Tourguide->bio)?:"you will enjoy your Time"}}</h6>
-                                  <div class="d-flex align-items-center justify-content-between mt-3 pb-3">
-                                      <div class="d-flex align-items-center justify-content-center foot">
-                                          <p class="admin">syndicate_No:{{$Tourguide->syndicate_No}} </p>
-
-                                        </div>
-                                  </div>
-                                  <div class="btn" data-bs-toggle="modal" data-bs-target="#myModal">Book now!</div>
-                              </div>
-                          </div>
-                      </div>
-                      @endforeach
+    <!-- HERO SECTION -->
+    <section class="home" id="home" style="background-image: url('./assets/asset/img/Old Cataract Hotel - Aswan 2.jpg')">
+        <div class="container">
+          <h1>Our Tourguide</h1>
+        </div>
+    </section>
+      <!-- HOTELS CARD -->
+       <section class="blog top" id="blog">
+        <div class="container">
+          <div class="heading"  >
+            <h5>OUR Tourguide</h5>
+            <h3>Discover your best stay in Aswan</h3>
+          </div>
+          <div class="content grid mtop" >
+            @foreach ($availableTourguides as $Tourguide)
+            <div class="box" style="width:23rem">
+              <!-- hotel img -->
+              <div class="img">
+                <img class="" src="  'img/'.{{ asset($Tourguide->User->image) }}" alt="">
+                <span>   {{$Tourguide->price_per_day}} L.E/Day</span>
+              </div>
+              <!-- hotel details -->
+              <div class="text">
+                <h2>{{$Tourguide->User->name}}</h2>
+                <p class="description" style="color:black">Language : </p>
+                @foreach( $Tourguide->languages as $lang)
+                <h1>  {{$lang->language}}</h1>
+                  @endforeach
+                  <p class="admin">syndicate_No:{{$Tourguide->syndicate_No}} </p>
+                  <h6 class="h-2 mt-4"><br> Bio:{{isset($Tourguide->bio)?:"you will enjoy your Time"}}</h6>
+                <a href="{{route('hotel.show',['id'=>$Hotel->id])}}">Show More<i class='far fa-long-arrow-alt-right'></i></a>
+            
+              </div>  
+                <!-- "hotels.hotel",["Hotel"=> $hotelInfo],["hotelImg"=>$hotelImgs] -->
+            </div>
+            @endforeach
           </div>
         </div>
-      </div>
-
-<!-- ===================Card================= -->
-
-<form action="{{route('getAvailablePlaces',['order'=>$order->id])}}">
-
-    <button type="submit" >back</button>
-</form>
-      </div>
-
-</div>
-
-
-
-
-<form action="{{route('MUTE')}}" method="POST">
-    @csrf
-    <div class="form-btn d-grid gap-2 d-md-flex justify-content-md-end">
-        <button class="submit-btn m-5" type="submit">skip &submit MUTE</button>
-      </div>
-</form>
-
-
-    </div>
-  </section>
-
-
-
-    </body>
+      </section> 
+  
+    
+</body>
 </html>
 
-
+@endsection
 
 
 
--- resources/views/allTourguide/allTourguide.blade.php
+++ resources/views/allTourguide/allTourguide.blade.php
@@ -1,4 +1,5 @@
-
+@extends("temp")
+@section('bodyContent')
 
 
 <!DOCTYPE html>
@@ -9,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
     <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    {{-- <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"> 
 
     <!-- Font Awesome -->
@@ -22,120 +23,78 @@
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{asset("./assets/home/css/style.css")}}" rel="stylesheet">
 <link rel="stylesheet" href="{{asset("./assets/css/index.css")}}">
-{{-- <link rel="stylesheet" href="{{asset('assets/css/Cards/tourguideCards.css')}}"> --}}
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> --}}
+<link rel="stylesheet" href="{{asset('assets/css/Cards/tourguideCards.css')}}"> --}}
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> 
     <title>Tourguide</title>
  </head>
 <body>
+    
 
-    @extends("temp")
-@section('bodyContent')
+{{-- ======================================================================================= --}}
 
-
-    <!-- ===================== -->
-
-    <!-- ===================== bagination ===================== -->
-
-    <!-- ===================== cards ===================== -->
-    <section>
-        <div class="container tourguideCard">
-            <div class="row g-5"  >
-                @foreach ($allTourGides as $Tourguide)
-                        <div class="col-lg-4  col-sm-6 col-12 rounded" >
-                            <div class="card border-0 me-lg-4 mb-lg-0 mb-4 " style="margin-top: 1rem;margin-button:1rem">
-                                <div class="backgroundEffect"></div>
-                                <div class="pic">
-                                    <img class="" src="  'img/'.{{ asset($Tourguide->User->image) }}" alt="">
-                                    <div class="date">
-                                        <span class="day">Book now!</span>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <h2>{{$Tourguide->User->name}}</h2>
-                                    <p class="text-muted mt-3">price {{$Tourguide->price_per_day}} /day</p> 
-                                        {{-- @foreach($Tourguide->languages as $item)
-                                            <h6 class="h-2 mt-4"><br>{{$item['language']}} </h6>
-                                        @endforeach  --}}
-                                    <p class="description" style="color:black">Language : </p>
-                                        @foreach($Tourguide->languages as $item)
-                                            <span class="description">{{$item['language']}},</span>
-                                        @endforeach
-                                    <h6 class="h-2 mt-4"><br> Bio:{{$Tourguide->bio}}</h6>
-                                    <div class="d-flex align-items-center justify-content-between mt-3 pb-3">
-                                        <div class="d-flex align-items-center justify-content-center foot">
-                                            <p class="admin">syndicate_No:{{$Tourguide->syndicate_No}} </p>
-                                        </div>
-                                    </div>
-                                    <div class="btn" data-bs-toggle="modal" data-bs-target="#myModal">Book now!</div>
-                                </div>
-                            </div> 
-                        </div>
-                        @endforeach
-            </div>
+<div class="container ">
+    <div class="row g-5"  >
+     @foreach ($allTourGides as $Tourguide)
+     <div class="col-lg-4  col-sm-6 col-12 rounded" >
+       <div class="card-deck">
+        <div class="card">
+          <img class="card-img-top" src="  'img/'.{{ asset($Tourguide->User->image) }}" alt="">
+        
+        <div class="card-body">
+          <h5 class="card-title">{{$Tourguide->User->name}}</h5>
+          <span class="card-text">syndicate_No:{{$Tourguide->syndicate_No}}</span>
+          <span>price {{$Tourguide->price_per_day}} L.E/day</span>
+          <p class="description" style="color:black">Language : </p>
+          @foreach($Tourguide->languages as $item)
+              <span class="description">{{$item['language']}},</span>
+          @endforeach
         </div>
-          <!-- The Modal -->
-          <div class="modal" id="myModal">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <!-- Modal Header -->
-                <div class="modal-header">
-                  <h4 class="modal-title">Book NOW!</h4>
-                  <!-- <button type="button" class="btn-close" data-bs-dismiss="modal"></button> -->
-                </div>
-                <!-- Modal body -->
-                <div class="modal-body" id="modal">
-                    <div id="booking" class="section">
-                        <div class="section-center">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-10">
-                                        <!-- col-md-push-5 -->
-                                        <div class="booking-cta mt-0">
-                                            <h3>Make your reservation</h3>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-10 ">
-                                        <!-- col-md-pull-7 -->
-                                        <div class="booking-form">
-                                            <form action="{{route('storeRegTourguide.store',['id'=>$Tourguide->id])}}" class="form" id="form2" method="POST">
-                                                @csrf
-                                                <div class="row">
-                                                    <div class="col-6">
-                                                        <div class="form-group">
-                                                            <span class="form-label"   >Check In</span>
-                                                            <input class="form-control" type="date" name="check_in" required>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-sm-6">
-                                                        <div class="form-group">
-                                                            <span class="form-label">Check out</span>
-                                                            <input class="form-control" type="date" name="check_out" required>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-btn">
-                                                    <button class="submit-btn bookbtnn"onclick() type="submit">Send a request</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Modal footer -->
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                </div>
-              </div>
-            </div>
-          </div>
-     </section>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+  @endforeach
+       </div>
+      </div>
+  </div>
+
  @endsection
 
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>  
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
