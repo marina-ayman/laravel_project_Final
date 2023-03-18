fda4b5ac0ac49be5958fe99221d784b5760a1677
Marina Ayman <99688015+marina-ayman@users.noreply.github.com>
2023-03-17 20:56:23 -0700

    marina

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -144,7 +144,6 @@
 
 <body>
 	
-<<<<<<< HEAD
 	
     <!-- Carousel Start -->
     <div class="container-fluid p-0">
@@ -184,8 +183,6 @@
         </div>
     </div>
     <!-- Carousel End -->
-=======
->>>>>>> 626eb73476e7a35976958fa91f5b9561b1c8a1c4
 
 
 
@@ -202,13 +199,8 @@
                 <div class="col-lg-6 pt-5 pb-lg-5">
                     <div class="about-text bg-white p-4 p-lg-5 my-lg-5">
                         <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">About Us</h6>
-<<<<<<< HEAD
                         <h1 class="mb-3">We Provide Best Tour Packages In Your Budget</h1>
                         <p></p>
-=======
-                        <h1 class="mb-1">We Provide Best Tour Packages In Your Budget</h1>
-                        <p>Dolores lorem lorem ipsum sit et ipsum. Sadip sea amet diam dolore sed et. Sit rebum labore sit sit ut vero no sit. Et elitr stet dolor sed sit et sed ipsum et kasd ut. Erat duo eos et erat sed diam duo</p>
->>>>>>> 626eb73476e7a35976958fa91f5b9561b1c8a1c4
                         <div class="row mb-4">
                             <div class="col-6">
                                 <img class="img-fluid" src="./assets/imgs/Aswan-Nilme.jpg" alt="" style="height: 12rem">
